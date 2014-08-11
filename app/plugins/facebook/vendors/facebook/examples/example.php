21e10bf7994ba75ea752eeb6e5008d2a7449b234
Shishir Singh <catchshishir@gmail.com>
2014-08-11 15:25:11 +0530

    Update example.php

--- app/plugins/facebook/vendors/facebook/examples/example.php
+++ app/plugins/facebook/vendors/facebook/examples/example.php
@@ -4,8 +4,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '117743971608120',
-  'secret' => '943716006e74d9b9283d4d5d8ab93204',
+  'appId'  => '',
+  'secret' => '',
   'cookie' => true,
 ));
 
